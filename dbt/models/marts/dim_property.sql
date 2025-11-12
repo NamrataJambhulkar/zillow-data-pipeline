cfde7fdbd7490d0d8dc3f2fad79190a93d959d4f
Namrata Jambhulkar <namratavj1@gmail.com>
2025-11-12 13:48:33 +0530

    Fixed data quality in dim_property model

--- dbt/models/marts/dim_property.sql
+++ dbt/models/marts/dim_property.sql
@@ -1,14 +1,19 @@
-{{ config(
-    materialized='table',
-) }}
-
-select distinct
-    address,
-    home_type,
-    area_sqft,
-    bedrooms,
-    bathrooms,
-    zestimate
-from {{ ref('stg_zillow_listings') }}
-where address is not null
-order by address
+with ranked as (
+    select
+        address,
+        coalesce(bedrooms, 0) as bedrooms,
+        coalesce(bathrooms, 0) as bathrooms,
+        coalesce(area_sqft, 0) as area_sqft,
+        city,
+        state,
+        zipcode,
+        home_type,
+        price,
+        zestimate,
+        row_number() over (partition by address order by price desc) as rn
+    from {{ ref('stg_zillow_listings') }}
+    where address is not null
+)
+select *
+from ranked
+where rn = 1
